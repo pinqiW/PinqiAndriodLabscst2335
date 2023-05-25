88e98aab6eca4a92e7b507ba5b5c836137d0ca61
Pinqi Wang <pinqints@gmail.com>
2023-05-25 16:57:47 -0400

    finished phone version of lab3

--- app/src/main/java/com/example/pinqisandriodlabs/MainActivity.java
+++ app/src/main/java/com/example/pinqisandriodlabs/MainActivity.java
@@ -3,18 +3,44 @@ package com.example.pinqisandriodlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.animation.Animation;
+import android.view.animation.LinearInterpolator;
+import android.view.animation.RotateAnimation;
+import android.widget.ImageView;
+import android.widget.Switch;
 
 public class MainActivity extends AppCompatActivity {
 
-
+    ImageView imgView;
+    Switch sw;
 
     // equivalent to    static void main(String args[])
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
+
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            imgView = findViewById(R.id.flagview);
+            sw = findViewById(R.id.spin_switch);
+
+            sw.setOnCheckedChangeListener( (btn, isChecked) -> {
+                if (isChecked)
+                {
+                    RotateAnimation rotate = new RotateAnimation(0, 360, Animation.RELATIVE_TO_SELF, 0.5f, Animation.RELATIVE_TO_SELF, 0.5f);
+                    rotate.setDuration(5000);
+                    rotate.setRepeatCount(Animation.INFINITE);
+                    rotate.setInterpolator(new LinearInterpolator());
+
+                    imgView.startAnimation(rotate);
+                }
+                else {
+                    imgView.clearAnimation();
+                }
 
 
-        // loads an XML file on the page
-        setContentView(R.layout.activity_main);
+            });
+
+
     }
 }
