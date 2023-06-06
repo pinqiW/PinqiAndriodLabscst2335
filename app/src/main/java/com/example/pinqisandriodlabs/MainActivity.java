912472587bd239a3889b9db51cb7c353cf9a8b08
Pinqi Wang <pinqints@gmail.com>
2023-06-06 17:11:58 -0400

    call number finished, working on change pictures.

--- app/src/main/java/com/example/pinqisandriodlabs/MainActivity.java
+++ app/src/main/java/com/example/pinqisandriodlabs/MainActivity.java
@@ -3,9 +3,12 @@ package com.example.pinqisandriodlabs;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.pinqisandriodlabs.databinding.ActivityMainBinding;
 
@@ -18,39 +21,69 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-        Log.e(TAG,"In OnCreate()");
+        Log.e(TAG, "In OnCreate()");
 
         binding = ActivityMainBinding.inflate(getLayoutInflater());
+
         // loads an XML file on the page
-        setContentView(R.layout.activity_main);
+        setContentView(binding.getRoot());
+//        setContentView(R.layout.activity_main);
 
-        binding.loginButton.setOnClickListener((v) -> {
-            Log.e(TAG,"You clicked the login Button");
+        Log.w(TAG, "In onCreate()-Loading Widgets");
 
-            //where to go :                   leaving here          going here
-            Intent nextPage = new Intent(this, SecondActivity.class);
+        binding.loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.e(TAG, "You clicked the login Button");
+                //            //where to go :                   leaving here          going here
+                Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
 
-            // go to another page
-            startActivity(nextPage);
+                String whatIsTyped = binding.emailText.getText().toString();
+                nextPage.putExtra("EMAIL",whatIsTyped);
+                nextPage.putExtra("AGE",25);
+                nextPage.putExtra("DAY", "Tuesday");
+
+                startActivity(nextPage); //carries all the data to the next page;
+
+
+
+//
+//                Intent intent2 = new Intent(Intent.ACTION_VIEW);
+//                intent2.setData(Uri.parse("https://www.algonquincollege.com"));
+
+//
+//                startActivity(call);
+            }
         });
+
+
     }
+
+
+
 
     @Override //garbage collected, app is gone
     protected void onDestroy() {
         super.onDestroy();
         Log.e(TAG,"In Destroy()");
+        Log.w(TAG,"In Destroy()- Any memory used by the application is freed");
+
     }
 
     @Override // now visible, not listening for clicks
     protected void onStart() {
         super.onStart();
         Log.e(TAG,"In OnStart()");
+        Log.w(TAG,"In onStart()-the application is now visible on screen");
+
     }
 
     @Override // no longer visible on screen
     protected void onResume() {
         super.onResume();
         Log.e(TAG,"In OnResume()");
+        Log.w(TAG,"In onResume()-The application is now responding to user input");
+
 
     }
 
@@ -58,5 +91,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onPause() {
         super.onPause();
         Log.e(TAG,"In OnPause()");
+        Log.w(TAG,"In onPause()-The application no longer responds to user input");
+
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w(TAG,"In Stop()-The application is no longer visible");
     }
 }
--- app/src/main/java/com/example/pinqisandriodlabs/SecondActivity.java
+++ app/src/main/java/com/example/pinqisandriodlabs/SecondActivity.java
@@ -1,16 +1,23 @@
 package com.example.pinqisandriodlabs;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 
 import com.example.pinqisandriodlabs.databinding.ActivitySecondBinding;
 
 public class SecondActivity extends AppCompatActivity {
 
     ActivitySecondBinding binding;
-
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,17 +25,53 @@ public class SecondActivity extends AppCompatActivity {
         binding = ActivitySecondBinding.inflate(getLayoutInflater());
 
         // loads the XML files /res/layout/activity_second.xml
-        setContentView(R.layout.activity_second);
+//        setContentView(R.layout.activity_second);
+       setContentView(binding.getRoot());
+
+        Intent fromPrevious = getIntent(); // return the Intent what got us here
+
+        // should have variable in fromPrevious
+        String EMAIL = fromPrevious.getStringExtra("EMAIL"); // doesn't need a defualtValue cuz its null by default.
+        int age = fromPrevious.getIntExtra("AGE",0);
+        String day = fromPrevious.getStringExtra("DAY");
+        int something = fromPrevious.getIntExtra("SOMETHING",0); // default is for when SOMETHING is not there
+
+        binding.textView4.setText("Welcome Back: "  + EMAIL );
+
+
+        binding.callButton.setOnClickListener((v)->{
+
+            String phoneNumToCall = binding.phoneToCall.getText().toString();
+            Intent call = new Intent(Intent.ACTION_DIAL);
+            call.setData(Uri.parse("tel:" + phoneNumToCall));
+            startActivity(call);
+        });
+
+        binding.changePicButton.setOnClickListener((v)->{
+            Intent cameraIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+
+            ActivityResultLauncher<Intent> cameraResult = registerForActivityResult(
+                    new ActivityResultContracts.StartActivityForResult()
+                    ,new ActivityResultCallback<ActivityResult>() {
+                        @Override
+                        public void onActivityResult(ActivityResult result) {
+                            if (result.getResultCode() == Activity.RESULT_OK) {
+
+                                Intent data = result.getData();
+                                Bitmap thumbnail = data.getParcelableExtra("data");
+                                profileImage.setImageBitmap(thumbnail);
+                            }
+                        }
+                    } );
+            cameraResult.launch(cameraIntent);
+
+        });
 
         binding.goBackButton.setOnClickListener((v)->{
-
             //opposite of startActivity
             finish(); //go back to previous
 
         });
-
-
-
 
 
 
