8ec6b7d8fd4cb0bbe5106066b404399f8ed49f47
Pinqi Wang <pinqints@gmail.com>
2023-07-03 21:56:20 -0400

    won't work after receive has been clicked

--- app/src/main/java/com/example/pinqisandriodlabs/ChatRoom.java
+++ app/src/main/java/com/example/pinqisandriodlabs/ChatRoom.java
@@ -2,72 +2,110 @@ package com.example.pinqisandriodlabs;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import com.example.pinqisandriodlabs.databinding.ActivityChatRoomBinding;
+import com.example.pinqisandriodlabs.databinding.ReceiveMessageBinding;
 import com.example.pinqisandriodlabs.databinding.SentMessageBinding;
 
-import org.w3c.dom.Text;
-
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
 public class ChatRoom extends AppCompatActivity {
 
-    ActivityChatRoomBinding binding;
-    ArrayList<String> messages = new ArrayList<>();
+    protected ArrayList<ChatMessage> messages = new ArrayList<>();
 
-    private RecyclerView.Adapter myAdapter;
+    protected Button myButton;
+    protected Button receButton;
+    protected RecyclerView recyclerView;
 
+
+    /** This holds the edit text for typing into */
+    protected EditText theTextInput;
+    ChatRoomViewModel model;
+    RecyclerView.Adapter myAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        model = new ViewModelProvider(this).get(ChatRoomViewModel.class);
+        ActivityChatRoomBinding binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
 
-        binding = ActivityChatRoomBinding.inflate(getLayoutInflater());
-//        setContentView(R.layout.activity_chat_room);
         setContentView(binding.getRoot());
 
-        binding.sendButton.setOnClickListener(click -> {
-            messages.add(binding.textInput.getText().toString());
-            myAdapter.notifyItemInserted(messages.size()-1);
+        myButton = binding.sendButton;
+        receButton = binding.receButton;
+        theTextInput = binding.textInput;
+        recyclerView = binding.recycleView;
 
-            //clear the previous text:
-            binding.textInput.setText("");
+        myButton.setOnClickListener(click -> {
+            String input = theTextInput.getText().toString();
+
+            boolean type = true;
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
+            String currentDateandTime = sdf.format(new Date());
+
+            //insert into ArrayList
+            messages.add(new ChatMessage(input, currentDateandTime, type));
+
+//            myAdapter.notifyItemInserted(messages.size()-1); //updates the rows
+            myAdapter.notifyDataSetChanged(); //updates the rows
+
+            //clear input
+            theTextInput.setText("");
         });
 
-//        The whole point of the MyRowHolder class is to maintain variables for what you want to set on each row in your list.
-        class MyRowHolder extends RecyclerView.ViewHolder {
-            TextView messageText;
-            TextView timeText;
+        receButton.setOnClickListener(click ->{
+            String input = theTextInput.getText().toString();
 
-            // The view that is passed in as a parameter represents the ConstraintLayout that is the root of the row.
-            public MyRowHolder(@NonNull View itemView) {
-//                This view has the two TextViews as sub-items so you have to find them by id, like you would normally do for an Activity in the onCreate function. Modify your constructor:
-                super(itemView);
-                messageText = itemView.findViewById(R.id.message);
-                timeText = itemView.findViewById(R.id.time);
-            }
-        }
+            boolean type = false;
+            SimpleDateFormat sdf = new SimpleDateFormat("EEEE, dd-MMM-yyyy hh-mm-ss a");
+            String currentDateandTime = sdf.format(new Date());
+
+            //insert into ArrayList
+            messages.add(new ChatMessage(input, currentDateandTime, type));
+
+//            myAdapter.notifyItemInserted(messages.size()-1); //updates the rows
+            myAdapter.notifyDataSetChanged(); //updates the rows
+
+
+            //clear input
+            theTextInput.setText("");
+
+        });
+
 
         /**
          * A RecyclerView.Adapter object needs 3 functions to tell the view how to draw items in the list.
          */
-        binding.recycleView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
+        recyclerView.setAdapter(myAdapter = new RecyclerView.Adapter<MyRowHolder>() {
             @NonNull
-
-            /**
-             *  This function creates a ViewHolder object which we'll learn next. It represents a single row in the list
-             *  This is responsible for creating a layout for a row, and setting the TextViews in code
-             */
-            @Override
             public MyRowHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-                SentMessageBinding binding = SentMessageBinding.inflate(getLayoutInflater());
-                return new MyRowHolder(binding.getRoot());
+                //this inflates the row layout
+
+                //int viewType is what layout to load
+                if(viewType == 1) {
+                    SentMessageBinding binding =                           //how big is parent?
+                            SentMessageBinding.inflate(getLayoutInflater(), parent, false);
+
+                    return new MyRowHolder(binding.getRoot());
+                }
+                else {
+                    ReceiveMessageBinding binding =
+                            ReceiveMessageBinding.inflate(getLayoutInflater(), parent, false);
+                    return new MyRowHolder(binding.getRoot());
+                }
             }
+
 
             /**
              * This initializes a ViewHolder to go at the row specified by the position parameter
@@ -77,10 +115,10 @@ public class ChatRoom extends AppCompatActivity {
              */
             @Override
             public void onBindViewHolder(@NonNull MyRowHolder holder, int position) {
-                holder.messageText.setText("");
-                holder.timeText.setText("");
-                String obj = messages.get(position);
-                holder.messageText.setText(obj);
+                //update the widgets:
+                ChatMessage atThisRow = messages.get(position);
+                holder.timeText.setText(atThisRow.timeSent);
+                holder.messageText.setText(atThisRow.message);//puts the string in position at theWord TextView
             }
 
             /**
@@ -92,7 +130,6 @@ public class ChatRoom extends AppCompatActivity {
                 return messages.size();
             }
 
-
             /**
              * load different layouts for different rows, this function lets you return an int to indicate which layout to load. For now, we are just loading one layout, so you can just return 0 for this function:
              * @param position position to query
@@ -100,10 +137,39 @@ public class ChatRoom extends AppCompatActivity {
              */
             @Override
             public int getItemViewType(int position){
-                return 0;
+                if(messages.get(position).sendOrReceive)
+                {
+                    return 1;
+                }
+                else
+                {
+                    return 2;
+                }
             }
-
         });
 
+        /**
+         * LayoutManager
+         * The RecyclerView supports 1 or more columns for showing data, and you can either scroll in a Vertical or Horizontal direction through the items.
+         * To specify a single column scrolling in a Vertical direction, you call:
+         */
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
     }
+
+
+
+    //        The whole point of the MyRowHolder class is to maintain variables for what you want to set on each row in your list.
+    class MyRowHolder extends RecyclerView.ViewHolder {
+        TextView messageText;
+        TextView timeText;
+
+        // The view that is passed in as a parameter represents the ConstraintLayout that is the root of the row.
+        public MyRowHolder(@NonNull View itemView) {
+            super(itemView);
+            messageText = itemView.findViewById(R.id.message);
+            timeText = itemView.findViewById(R.id.time);
+        }
+    }
+
 }
